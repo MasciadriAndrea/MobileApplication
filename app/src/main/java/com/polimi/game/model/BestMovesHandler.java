1db01226469d46c7e27df1b5e1db2806bb6f75ae
andrea_masciadri <masciadri.andrea@gmail.com>
2014-12-11 12:05:47 +0100

    changed ArrayList in List

--- app/src/main/java/com/polimi/game/model/BestMovesHandler.java
+++ app/src/main/java/com/polimi/game/model/BestMovesHandler.java
@@ -1,12 +1,13 @@
 package com.polimi.game.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class BestMovesHandler {
 
     //TODO Class methods not yet implemented
 
-    private ArrayList<BestMoveResult> tenBest;
+    private List<BestMoveResult> tenBest;
 
     public BestMovesHandler(ArrayList<BestMoveResult> tenBest) {
         this.tenBest = tenBest;
--- app/src/main/java/com/polimi/game/model/Board.java
+++ app/src/main/java/com/polimi/game/model/Board.java
@@ -1,10 +1,11 @@
 package com.polimi.game.model;
 
-import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Board {
-    private ArrayList<Container> containers;
+    private List<Container> containers;
     private static Integer nIni=3;
 
     public Board(Player p1, Player p2) {
@@ -63,7 +64,7 @@ public class Board {
 
     private void buildBoard(int[] initialBoard, Player p1, Player p2){
         int[] opposite={0,8,14,13,12,11,10,9,1,7,6,5,4,3,2};
-        ArrayList cont=new ArrayList<Container>();
+        List cont=new ArrayList<Container>();
         this.containers=cont;
         Tray t1=new Tray(1,initialBoard[0],p1,null);
         Container c= (Container) t1;
@@ -85,7 +86,7 @@ public class Board {
         t1.setNextContainer(c);
     }
 
-    public ArrayList getContainers(){
+    public List getContainers(){
         return this.containers;
     }
 }
