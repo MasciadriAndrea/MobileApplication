8bd7fa6d1ef82b7611111890a2ce356b1f258910
andrea_masciadri <masciadri.andrea@gmail.com>
2014-12-23 15:28:58 +0100

    created loadActivity

---
+++ app/src/main/java/it/polimi/activities/LoadActivity.java
@@ -0,0 +1,29 @@
+package it.polimi.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.polimi.core.R;
+
+import it.polimi.core.Assets;
+
+public class LoadActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_load);
+        Assets.load();
+        Intent i = new Intent(this,MenuActivity.class);
+        startActivity(i);
+    }
+}
