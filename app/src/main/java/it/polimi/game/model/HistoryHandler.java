17da1552dc106ce1e4fa099deb54a87851f5adb3
andrea_masciadri <masciadri.andrea@gmail.com>
2015-01-14 11:33:07 +0100

    modify history

--- app/src/main/java/it/polimi/game/model/HistoryHandler.java
+++ app/src/main/java/it/polimi/game/model/HistoryHandler.java
@@ -21,16 +21,15 @@ public class HistoryHandler {
         return instance;
     }
 
+    public void addGameHistory(GameHistory gh){
+        HistoryDAO.getInstance().addGameHistory(gh);
+        this.setGames();
+    }
+
     public void setGames(){
         List<GameHistory> gameHistories=new ArrayList<GameHistory>();
 
         gameHistories = HistoryDAO.getInstance().getLastGameHistory();
-        //TODO take g from database
-        //TODO to test i will try this--- to be deleted
-        //g.add(new GameHistory(PlayerHandler.getInstance().getPlayerById(2),PlayerHandler.getInstance().getPlayerById(3),3,4,new Date(33)));
-        //g.add(new GameHistory(PlayerHandler.getInstance().getPlayerById(1),PlayerHandler.getInstance().getPlayerById(3),33,4,new Date(433)));
-        //g.add(new GameHistory(PlayerHandler.getInstance().getPlayerById(3),PlayerHandler.getInstance().getPlayerById(2),3,24,new Date(533)));
-        //TODO remove since this
         this.games=gameHistories;
     }
     public List<GameHistory> getGames(){
